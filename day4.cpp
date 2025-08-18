fc304ff5b14dd0e3d7791660429e25df2fa7fcb9
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-08-18 10:24:08 +0530

    day 4 completed

---
+++ day4.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(vector<int> arr1, vector<int> arr2, int n, int m) {
+    int left = n - 1; //at last of array 1 
+    int right = 0;    //at first of array 2
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--;
+            right++;
+        }
+        else {
+            break;
+        }
+    }
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+}
+int main(){
+    vector<int> arr1={1,3,5,7};
+    vector<int> arr2={2,4,6,8};
+    int n = arr1.size(), m = arr2.size();
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "\narr1 = ";
+    for (int i = 0; i < n; i++) {
+        if(i==n-1) cout<<arr1[i];
+        else cout << arr1[i] << " , ";
+    }
+    cout << "\narr2 = ";
+    for (int i = 0; i < m; i++) {
+        if(i==m-1) cout<<arr2[i];
+        else cout << arr2[i] << " , ";
+    }
+    return 0;
+}
