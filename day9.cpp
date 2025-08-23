9a43585f63a185a4b4191528c7cb4349d260b9ba
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-08-23 19:34:59 +0530

    day 9 done

---
+++ day9.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string commonPrefix(vector<string>& arr,int n){
+    string s=" "; //stores mini string
+    int mini=INT_MAX;
+    for(int i=0;i<n;i++){
+        int len=arr[i].size();
+        if(len<mini){
+            mini=len;
+            s=arr[i];
+        }
+    }
+    int cnt=0;
+    int flag=0;
+    for(int i=0;i<mini;i++){
+        for(int j=0;j<n;j++){
+            if(arr[j][i]!=s[i]){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1){
+            break;
+        }
+        cnt++;
+    }
+    return s.substr(0,cnt);
+}
+int main(){
+    vector<string> s={{"flower"},{"flow"},{"flight"}};
+    int n= s.size();
+	string ans = commonPrefix(s,n);
+    if(ans.size()==0) cout<<"";
+    else cout<<ans;
+    return 0;
+}
